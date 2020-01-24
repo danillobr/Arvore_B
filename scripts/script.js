fda2cc8a9ae899053fcdcb41c9caadf0c5b53466
danilo100kl <danilo100kl@hotmail.com>
2020-01-23 23:15:44 -0300

    Atualizações

--- scripts/script.js
+++ scripts/script.js
@@ -106,8 +106,8 @@ function ArvoreB(ord) {
     this.raiz = new No(this.ordem);
     this.qtd_chaves = 0;
 
-    this.x_offset = 0;
-    this.y_offset = 0;
+    this.x_deslocamento = 0;
+    this.y_deslocamento = 0;
     /*
      inserir_chave: insere uma nova chave na árvore
      chave: a nova chave a ser inserida
@@ -154,11 +154,11 @@ function ArvoreB(ord) {
     }
 
     /*
-    search_key: pesquisa a existência e posição de uma chave na árvore
+    buscar_chave: pesquisa a existência e posição de uma chave na árvore
     chave: o valor a ser pesquisado.
     Retorna: o nó que contém a chave e o índice no diretório da matriz de chaves ou [nulo, 0] se a chave não for encontrada.
     */
-    this.search_key = function (chave) {
+    this.buscar_chave = function (chave) {
         return this.raiz.buscar(chave)
     }
 }
@@ -168,232 +168,233 @@ DesenhaArvoreB: um desenho de um objeto ArvoreB
 árvore: o ArvoreB a ser representado
 tela: uma tela na qual a árvore será representada
 */
-function DesenhaArvoreB(tree, canvas) {
-    this.tree = tree;
+function DesenhaArvoreB(arvore, canvas) {
+    this.arvore = arvore;
     this.canvas = canvas;
-    this.context = canvas.getContext("2d");
-    this.highlight = null;
+    this.contexto = canvas.getContext("2d");
+    this.realcar = null;
 
 
     /*
-    draw: (re) pinta a árvore
-         mode: descreve o motivo do (re) desenho
+    desenhar: (re) pinta a árvore
+         modo: descreve o motivo do (re) desenho
          centro_raiz: centralizando a raiz na parte superior
          centro_no: centralizar totalmente um nó e, opcionalmente, destacando-o
          rolagem: rolando a árvore, mova-a
-         Para cada modo, arg1 e arg2 assumem valores diferentes:
-         centro_raiz: arg1 e arg2 não são usados;
-         centro_no: arg1 é o nó central e arg2 é a chave de destaque
+         Para cada modo, arg1 e arg2 assumem valores diferentes:
+            centro_raiz: arg1 e arg2 não são usados;
+            centro_no: arg1 é o nó central e arg2 é a chave de destaque
          rolagem: arg1 é o delta x e arg2 é o delta y
      Devoluções: -
     */
-    this.draw = function (mode, arg1, arg2) {
-        if (mode == undefined)
-            mode = centro_raiz;
+    this.desenhar = function (modo, arg1, arg2) {
+        if (modo == undefined)
+            modo = centro_raiz;
 
         // Localize variáveis para facilitar o acesso
         var canvas = this.canvas;
-        var context = this.context;
+        var contexto = this.contexto;
 
         canvas.width = canvas.width;
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        context.textAlign = "center";
-        context.textBaseline = "middle";
-        context.font = "12px arial";
+        contexto.clearRect(0, 0, canvas.width, canvas.height);
+        contexto.textAlign = "center";
+        contexto.textBaseline = "middle";
+        contexto.font = "12px arial";
 
         // Centralize na raiz se centro_no for indefinido
         // A raiz está centralizada de maneira diferente: no centro, na parte superior
         var deltas;
-        if (mode != rolagem) {
-            function get_delta_to_point(No, x, y) {
-                var middle = (No.chaves.length * largura_chave) / 2;
-                return [x - No.x - middle, y - No.y - altura_chave / 2];
+        if (modo != rolagem) {
+            function pegar_pontos_do_delta(No, x, y) {
+                var meio = (No.chaves.length * largura_chave) / 2;
+                return [x - No.x - meio, y - No.y - altura_chave / 2];
             }
 
-            this.position_tree(this.tree.raiz);
-            if (mode == centro_raiz)
-                deltas = get_delta_to_point(this.tree.raiz,
+            this.posicao_arvore(this.arvore.raiz);
+            if (modo == centro_raiz)
+                deltas = pegar_pontos_do_delta(this.arvore.raiz,
                     canvas.width / 2,
                     deslocamento * 2 + 8);
-            else if (mode == centro_no)
-                deltas = get_delta_to_point(arg1,
+            else if (modo == centro_no)
+                deltas = pegar_pontos_do_delta(arg1,
                     canvas.width / 2,
                     canvas.height / 2);
-            this.move_tree(deltas[0], deltas[1]);
-            this.highlight = null;
+            this.mover_arvore(deltas[0], deltas[1]);
+            this.realcar = null;
         }
         else {
-            this.move_tree(arg1 * 5, arg2 * 5);
+            this.mover_arvore(arg1 * 5, arg2 * 5);
             // Se estiver rolando, mantenha o realce
-            if (this.highlight != null) {
-                mode = centro_no;
-                arg1 = this.highlight[0];
-                arg2 = this.highlight[1];
+            if (this.realcar != null) {
+                modo = centro_no;
+                arg1 = this.realcar[0];
+                arg2 = this.realcar[1];
             }
         }
-        this.draw_No(this.tree.raiz, context);
 
-        if (mode == centro_no) {
-            context.lineWidth = 2;
-            context.strokeStyle = "#F57900"; // lembrete: cor laranja
-            context.strokeRect(arg1.x + arg2 * largura_chave,
+        this.desenhar_no(this.arvore.raiz, contexto);
+
+        if (modo == centro_no) {
+            contexto.lineWidth = 2;
+            contexto.strokeStyle = "#F57900"; // lembrete: cor laranja
+            contexto.strokeRect(arg1.x + arg2 * largura_chave,
                 arg1.y, largura_chave, altura_chave);
-            this.highlight = [arg1, arg2];
+            this.realcar = [arg1, arg2];
         }
 
         // (Re) desenhe o quadro da tela
-        context.fillStyle = "#555753"; // lembrete: cor Alumínio
-        context.beginPath();
+        contexto.fillStyle = "#555753"; // lembrete: cor Alumínio
+        contexto.beginPath();
         // Seta superior
-        context.clearRect(0, 0, canvas.width, deslocamento + 4);
-        context.moveTo(canvas.width / 2, 0);
-        context.lineTo((canvas.width / 2) - (deslocamento / 2), deslocamento);
-        context.lineTo((canvas.width / 2) + (deslocamento / 2), deslocamento);
+        contexto.clearRect(0, 0, canvas.width, deslocamento + 4);
+        contexto.moveTo(canvas.width / 2, 0);
+        contexto.lineTo((canvas.width / 2) - (deslocamento / 2), deslocamento);
+        contexto.lineTo((canvas.width / 2) + (deslocamento / 2), deslocamento);
 
         // Seta da esquerda
-        context.clearRect(0, 0, deslocamento + 4, canvas.height);
-        context.moveTo(0, canvas.height / 2);
-        context.lineTo(deslocamento, canvas.height / 2 + (deslocamento / 2));
-        context.lineTo(deslocamento, canvas.height / 2 - (deslocamento / 2));
+        contexto.clearRect(0, 0, deslocamento + 4, canvas.height);
+        contexto.moveTo(0, canvas.height / 2);
+        contexto.lineTo(deslocamento, canvas.height / 2 + (deslocamento / 2));
+        contexto.lineTo(deslocamento, canvas.height / 2 - (deslocamento / 2));
 
         // Seta de inferior
-        context.clearRect(0, canvas.height - deslocamento - 4,
+        contexto.clearRect(0, canvas.height - deslocamento - 4,
             canvas.width, canvas.height);
-        context.moveTo(canvas.width / 2, canvas.height);
-        context.lineTo((canvas.width / 2) - (deslocamento / 2),
+        contexto.moveTo(canvas.width / 2, canvas.height);
+        contexto.lineTo((canvas.width / 2) - (deslocamento / 2),
             canvas.height - deslocamento);
-        context.lineTo((canvas.width / 2) + (deslocamento / 2),
+        contexto.lineTo((canvas.width / 2) + (deslocamento / 2),
             canvas.height - deslocamento);
 
         // Seta da direita
-        context.clearRect(canvas.width - deslocamento - 4, 0,
+        contexto.clearRect(canvas.width - deslocamento - 4, 0,
             canvas.width, canvas.height);
-        context.moveTo(canvas.width, canvas.height / 2);
-        context.lineTo(canvas.width - deslocamento,
+        contexto.moveTo(canvas.width, canvas.height / 2);
+        contexto.lineTo(canvas.width - deslocamento,
             canvas.height / 2 + (deslocamento / 2));
-        context.lineTo(canvas.width - deslocamento,
+        contexto.lineTo(canvas.width - deslocamento,
             canvas.height / 2 - (deslocamento / 2));
 
-        context.closePath();
-        context.fill();
+        contexto.closePath();
+        contexto.fill();
     }
 
     /*
-    draw_No: desenha uma árvore enraizada no nó fornecido em um contexto de tela.
+    desenhar_no: desenha uma árvore enraizada no nó fornecido em um contexto de tela.
          nó: o nó que será desenhado
     Devoluções: -
     */
-    this.draw_No = function (No) {
+    this.desenhar_no = function (No) {
         // Localize variáveis para facilitar o acesso
-        var context = this.context;
+        var contexto = this.contexto;
 
         // Desenha o nó
-        context.lineWidth = 2;
-        context.strokeStyle = "#729FCF"; // lembrete: azul céu
-        context.fillStyle = "#000000";
+        contexto.lineWidth = 2;
+        contexto.strokeStyle = "#729FCF"; // lembrete: azul céu
+        contexto.fillStyle = "#000000";
 
-        var key;
-        var step = largura_chave;
+        var chave;
+        var passo = largura_chave;
         for (var i = 0; i < No.chaves.length; i++) {
-            key = No.chaves[i];
-            context.strokeRect(No.x + i * step, No.y, largura_chave, altura_chave);
-            context.fillText(key, (No.x + i * step) + (largura_chave / 2),
+            chave = No.chaves[i];
+            contexto.strokeRect(No.x + i * passo, No.y, largura_chave, altura_chave);
+            contexto.fillText(chave, (No.x + i * passo) + (largura_chave / 2),
                 No.y + (altura_chave / 2));
         }
 
         // Desenhar e conectar os filhos
-        var child;
-        context.strokeStyle = "#73D216"; // lembrete: cor verde qualquer
+        var filho;
+        contexto.strokeStyle = "#73D216"; // lembrete: cor verde qualquer
         for (var i = 0; i < No.filhos.length; i++) {
-            context.beginPath();
-            child = No.filhos[i];
-            this.draw_No(child, context);
-            context.moveTo(No.x + i * step, No.y + altura_chave);
-            context.lineTo(child.x + ((child.chaves.length * largura_chave) / 2),
-                child.y - 1);
-            context.lineWidth = 1;
-            context.stroke();
-            context.closePath();
+            contexto.beginPath();
+            filho = No.filhos[i];
+            this.desenhar_no(filho, contexto);
+            contexto.moveTo(No.x + i * passo, No.y + altura_chave);
+            contexto.lineTo(filho.x + ((filho.chaves.length * largura_chave) / 2),
+                filho.y - 1);
+            contexto.lineWidth = 1;
+            contexto.stroke();
+            contexto.closePath();
         }
     }
 
     /*
-    get_tree_width: calcula a largura de uma árvore com base na extremidade esquerda e filhos mais à direita.
+    pegar_largura_arvore: calcula a largura de uma árvore com base na extremidade esquerda e filhos mais à direita.
     nó: a raiz da árvore cuja largura será calculada
     Retorna: a largura da árvore.
     */
-    this.get_tree_width = function (No) {
-        if (No.filhos.length > 0) {
-            var lm = No.filhos[0];
+    this.pegar_largura_arvore = function (no) {
+        if (no.filhos.length > 0) {
+            var lm = no.filhos[0];
             while (lm.filhos.length > 0)
                 lm = lm.filhos[0];
-            var rm = No.filhos[No.filhos.length - 1];
+            var rm = no.filhos[no.filhos.length - 1];
             while (rm.filhos.length > 0)
                 rm = rm.filhos[rm.filhos.length - 1];
             return (rm.x - lm.x) + largura_chave * rm.chaves.length;
         }
-        return largura_chave * No.chaves.length;
+        return largura_chave * no.chaves.length;
     }
 
     /*
-    position_tree: coloque uma árvore para desenhar.
-         nó: a raiz da árvore a ser apresentada
-         pNo: o nó pai (o padrão é indefinido)
+    posicao_arvore: coloque uma árvore para desenhar.
+         no: a raiz da árvore a ser apresentada
+         no_pai: o nó pai (o padrão é indefinido)
          cur_x: a coordenada x atual (o padrão é 0)
     Devoluções: -
     */
-    this.position_tree = function (No, pNo, cur_x) {
-        if (No.filhos.length != 0) {
+    this.posicao_arvore = function (no, no_pai, cur_x) {
+        if (no.filhos.length != 0) {
             cur_x = (cur_x == undefined) ? 0 : cur_x;
             // Ajuste a coordenada y para colocar o nó abaixo do seu
             // pai
-            if (pNo != undefined)
-                No.y = (pNo.y + altura_chave) + espaco_entre_nos;
+            if (no_pai != undefined)
+                no.y = (no_pai.y + altura_chave) + espaco_entre_nos;
             // Para cada criança ...
-            var child;
-            for (var i = 0; i < No.filhos.length; i++) {
-                child = No.filhos[i];
+            var filho;
+            for (var i = 0; i < no.filhos.length; i++) {
+                filho = no.filhos[i];
 
                 // Se não for uma folha, coloque o sutree em conformidade                
-                if (!child.folha())
-                    this.position_tree(child, No, cur_x);
+                if (!filho.folha())
+                    this.posicao_arvore(filho, no, cur_x);
                 // Se é uma folha, disponha-a com base em seus irmãos
                 else {
-                    child.x = cur_x;
-                    child.y = No.y + altura_chave + espaco_entre_nos;
+                    filho.x = cur_x;
+                    filho.y = no.y + altura_chave + espaco_entre_nos;
                 }
                 // Aumente a coordenada x com base na largura da sutree
-                cur_x += this.get_tree_width(child) + espaco_entre_nos;
+                cur_x += this.pegar_largura_arvore(filho) + espaco_entre_nos;
             }
             // Ajuste a coordenada x para centralizar acima dos filhos
-            if (!No.folha()) {
-                var first = No.filhos[0];
-                var last = No.filhos[No.filhos.length - 1];
-                var width = No.chaves.length * largura_chave;
-                No.x = ((first.x + last.x +
-                    (last.chaves.length) * largura_chave) / 2) - width / 2;
+            if (!no.folha()) {
+                var primeiro = no.filhos[0];
+                var ultimo = no.filhos[no.filhos.length - 1];
+                var largura = no.chaves.length * largura_chave;
+                no.x = ((primeiro.x + ultimo.x +
+                    (ultimo.chaves.length) * largura_chave) / 2) - largura / 2;
             }
         }
     }
 
     /*
-    move_tree: move os nós de uma árvore com base em determinados deltas
+    mover_arvore: move os nós de uma árvore com base em determinados deltas
          nó: o nó a ser movido (se indefinido, torna-se a raiz)
          delta_x: delta x (horizontal)
          delta_y: delta y (vertical)
     Devoluções: -
     */
-    this.move_tree = function (delta_x, delta_y, No) {
-        if (No == undefined) {
-            No = this.tree.raiz;
-            this.offset_x += delta_x;
-            this.offset_y += delta_y;
+    this.mover_arvore = function (delta_x, delta_y, no) {
+        if (no == undefined) {
+            no = this.arvore.raiz;
+            this.deslocamento_x += delta_x;
+            this.deslocamento_y += delta_y;
         }
 
-        No.x += delta_x;
-        No.y += delta_y;
-        for (var i = 0; i < No.filhos.length; i++)
-            this.move_tree(delta_x, delta_y, No.filhos[i]);
+        no.x += delta_x;
+        no.y += delta_y;
+        for (var i = 0; i < no.filhos.length; i++)
+            this.mover_arvore(delta_x, delta_y, no.filhos[i]);
     }
 }
 
@@ -402,28 +403,28 @@ function on_botao_inserir_chave_clicked() {
     var chave = parseInt(document.getElementById("entrada_inserir_chave").value);
     if (chave || chave == 0) {
         arvore.inserir_chave(chave);
-        drawing.draw();
+        desenho.desenhar();
     }
 }
 
 function on_botao_buscar_chave_clicked() {
-    var n = parseInt(document.getElementById("search_key").value);
-    if (isNaN(n))
+    var chave = parseInt(document.getElementById("buscar_chave").value);
+    if (isNaN(chave))
         alert("A chave deve ser um inteiro");
     else {
-        var result, No, position;
-        result = arvore.search_key(n);
-        No = result[0]; hl_position = result[1];
-        if (No != null) {
-            drawing.draw(centro_no, No, hl_position);
+        var resultado, no, posicao;
+        resultado = arvore.buscar_chave(chave);
+        no = resultado[0]; posicao = resultado[1];
+        if (no != null) {
+            desenho.desenhar(centro_no, no, posicao);
         }
         else
-            alert("A chave " + n + " não está na árvore");
+            alert("A chave " + chave + " não está na árvore");
     }
 }
 
 function on_botao_limpar_arvore_clicked() {
-    nova_arvore(tree.ordem);
+    nova_arvore(arvore.ordem);
 }
 
 function on_botao_criar_arvore_clicked() {
@@ -458,33 +459,33 @@ function on_canvas_clicked(e) {
     var delta_y = 0;
     var width = canvas.width;
     var height = canvas.height;
-    middle_y = canvas.height / 2;
-    middle_x = canvas.width / 2;
+    meio_y = canvas.height / 2;
+    meio_x = canvas.width / 2;
     // Rolar para a esquerda
     if (x < deslocamento * 2 &&
-        middle_y - deslocamento <= y && y <= middle_y + deslocamento)
+        meio_y - deslocamento <= y && y <= meio_y + deslocamento)
         delta_x = deslocamento;
     // Rolar para a direita
     else if (x > width - deslocamento * 2 &&
-        middle_y - deslocamento <= y && y <= middle_y + deslocamento)
+        meio_y - deslocamento <= y && y <= meio_y + deslocamento)
         delta_x = -deslocamento;
     // Rolar para baixo
     else if (y < deslocamento * 2 &&
-        middle_x - deslocamento <= x && x <= middle_x + deslocamento)
+        meio_x - deslocamento <= x && x <= meio_x + deslocamento)
         delta_y = deslocamento;
     // Rolar para cima
     else if (y > height - deslocamento * 2 &&
-        middle_x - deslocamento <= x && x <= middle_x + deslocamento)
+        meio_x - deslocamento <= x && x <= meio_x + deslocamento)
         delta_y = -deslocamento;
-    drawing.draw(rolagem, delta_x, delta_y);
+    desenho.desenhar(rolagem, delta_x, delta_y);
 }
 
 /* Cria uma nova árvore */
 function nova_arvore(ordem) {
     arvore = new ArvoreB(ordem);
     var canvas = document.getElementById("canvas");
-    drawing = new DesenhaArvoreB(arvore, canvas);
-    drawing.draw();
+    desenho = new DesenhaArvoreB(arvore, canvas);
+    desenho.desenhar();
 }
 
 /* Libera os botões de inserir e remover chave para o uso */
